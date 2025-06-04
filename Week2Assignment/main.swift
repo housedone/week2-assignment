29f2d9346c1aba3ee2f2924ad5361edeaca79da6
Wooseong <thehousedone@gmail.com>
2025-06-04 14:36:04 +0900

    feat: 필수 문제 1 작성

--- Week2Assignment/main.swift
+++ Week2Assignment/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+/// 필수 문제 1
+let sum: (Int, Int) -> String = { a, b in
+    "두 수의 합은 \(a + b) 입니다."
+}
+
+print(sum(10, 20))
+
+func calculate(closure: (Int, Int) -> String) {
+    let result = sum(10, 20)
+    
+    print(result)
+}
+
+calculate(closure: sum)
+
+
+/// 필수 문제 2
 
