d2cb1f9bd7ed97225906e83090cb19617e63e553
Wooseong <thehousedone@gmail.com>
2025-06-09 22:38:54 +0900

    chore: 간단한 수정

--- Week2Assignment/problem01.swift
+++ Week2Assignment/problem01.swift
@@ -9,16 +9,13 @@ import Foundation
 
 /// 필수 문제 1
 func problem01() {
-    
     // 1. 클로저 설계
     let sum: (Int, Int) -> String = { a, b in
         "두 수의 합은 \(a + b) 입니다."
     }
     
-    
     // 2. sum 호출
     print(sum(10, 20))
-    
     
     // 3. sum과 같은 타입의 클로저를 파라미터로 받는 함수
     func calculate(closure: (Int, Int) -> String) {
